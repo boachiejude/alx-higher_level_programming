89d7ef2c79cc7d0adcc4dc0350576ef13bc319fd
Jude Boachie <judeboachiekwame@gmail.com>
2023-03-07 15:15:36 +0000

    fix 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-new_str = str[39:76] + "\n"
-print(new_str)
+print(str[39:76] + "\n")
