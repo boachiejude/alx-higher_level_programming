72a1b5cd37a6dae15bd4b71b8077358161fa43f4
Jude Boachie <judeboachiekwame@gmail.com>
2023-03-08 15:27:29 +0000

    add files via upload

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """adds two integers and returns the result
+
+    Args:
+        a (int, float): first number
+        b (int, float): second number
+
+    Returns:
+        interger, float: the sum of a and b
+    """
+    return a + b
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """computes a to the power of b and return the value
+
+    Args:
+        a (int, float): first number
+        b (int, float): second number
+
+    Returns:
+        integer, float: a to the power of b
+    """
+    return a ** b
