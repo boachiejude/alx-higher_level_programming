ec9bd450a649787b1c98855f77a35c7a68e2e3a0
Jude Boachie <judeboachiekwame@gmail.com>
2023-03-08 15:00:43 +0000

    update: use less print statements

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{:c}".format(ord(str[i]) - 32), end="")
+def uppercase(string):
+    for i in range(len(string)):
+        if ord(string[i]) >= 97 and ord(string[i]) <= 122:
+            print("{:c}".format(ord(string[i]) - 32), end="")
         else:
-            print("{:c}".format(ord(str[i])), end="")
-    print()
+            print("{:c}".format(ord(string[i])), end="")
